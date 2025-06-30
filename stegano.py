e7ce5adef9e6e2328ff768549fd08d4b5b3f15b2
AkbarOktaviadi89 <Akbaroktaviadi89@gmail.com>
2025-06-30 23:33:19 +0700

    Feat: ad ddecode image function

--- stegano.py
+++ stegano.py
@@ -33,3 +33,26 @@ def encode_image(image_path, message, output_path):
     encoded.save(output_path)
     print(f"[+] Pesan berhasil disisipkan ke: {output_path}")
 
+
+def decode_image(image_path):
+    img = Image.open(image_path)
+    width, height = img.size
+    binary_data = ""
+    for y in range(height):
+        for x in range(width):
+            r, g, b = img.getpixel((x, y))
+            binary_data += str(r & 1)
+            binary_data += str(g & 1)
+            binary_data += str(b & 1)
+
+    all_bytes = [binary_data[i:i+8] for i in range(0, len(binary_data), 8)]
+    decoded_message = ""
+    for byte in all_bytes:
+        char = chr(int(byte, 2))
+        if char == chr(0):
+            break
+        decoded_message += char
+
+    print("[+] Pesan ditemukan:")
+    print(decoded_message)
+
